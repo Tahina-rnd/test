7e278045f3971a0f0295076872314a8b82651d94
Tahina-rnd <rtahinaa@gmail.com>
2026-01-11 11:45:54 +0300

    mini

--- srcs/parsing/expander/expander.c
+++ srcs/parsing/expander/expander.c
@@ -6,66 +6,139 @@
 /*   By: tarandri <tarandri@student.42antananarivo. +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/26 07:44:36 by tarandri          #+#    #+#             */
-/*   Updated: 2026/01/11 11:16:30 by tarandri         ###   ########.fr       */
+/*   Updated: 2026/01/11 11:41:12 by tarandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/parsing.h"
 
-static void	process_segment(t_arg *arg, t_segment *seg, t_shell *shell)
-{
-	char	*expanded;
-	char	*old_val;
+/* --- PARTIE 1 : UTILITAIRES LISTE ARGUMENTS --- */
 
-	if (seg->quote == QUOTE_SINGLE)
-		expanded = ft_strdup(seg->value);
-	else
-		expanded = expand_text(seg->value, shell);
-	if (!expanded)
-		return ;
-	old_val = arg->value;
-	arg->value = ft_strjoin_free(old_val, expanded);
-	free(expanded);
+static t_arg	*new_arg_node(void)
+{
+	t_arg	*node;
+
+	node = malloc(sizeof(t_arg));
+	if (!node)
+		return (NULL);
+	node->value = NULL;
+	node->next = NULL;
+	node->segments = NULL;
+	return (node);
 }
 
-static void	expand_arg(t_arg *arg, t_shell *shell)
+static void	append_val(t_arg *arg, char *str)
 {
-	t_segment	*seg;
+	char	*tmp;
 
-	if (arg->value)
-		free(arg->value);
-	arg->value = ft_strdup("");
+	if (!arg->value)
+		arg->value = ft_strdup("");
+	tmp = arg->value;
+	arg->value = ft_strjoin_free(tmp, str);
+}
+
+/* --- PARTIE 2 : LOGIQUE WORD SPLITTING --- */
+
+static t_arg	*handle_unquoted(t_arg *curr, char *val)
+{
+	int		i;
+	char	buf[2];
+
+	i = 0;
+	buf[1] = '\0';
+	while (val[i])
+	{
+		if (val[i] == ' ' || val[i] == '\t' || val[i] == '\n')
+		{
+			if (curr->value != NULL)
+			{
+				curr->next = new_arg_node();
+				curr = curr->next;
+			}
+			while (val[i] && (val[i] == ' ' || val[i] == '\t' || val[i] == '\n'))
+				i++;
+			continue ;
+		}
+		buf[0] = val[i];
+		append_val(curr, buf);
+		i++;
+	}
+	return (curr);
+}
+
+static t_arg	*expand_arg_to_list(t_arg *arg, t_shell *shell)
+{
+	t_arg		*head;
+	t_arg		*curr;
+	t_segment	*seg;
+	char		*exp_val;
+
+	head = new_arg_node();
+	curr = head;
 	seg = arg->segments;
 	while (seg)
 	{
-		process_segment(arg, seg, shell);
+		if (seg->quote == QUOTE_SINGLE)
+			append_val(curr, seg->value);
+		else
+		{
+			exp_val = expand_text(seg->value, shell);
+			if (seg->quote == QUOTE_DOUBLE)
+				append_val(curr, exp_val);
+			else
+				curr = handle_unquoted(curr, exp_val);
+			free(exp_val);
+		}
 		seg = seg->next;
 	}
+	if (curr->value == NULL && head == curr)
+	{
+		free(head);
+		return (NULL);
+	}
+	return (head);
 }
+
+static void	splice_args(t_command *cmd, t_arg *prev, t_arg *curr, t_arg *new_l)
+{
+	t_arg	*last_new;
+
+	if (new_l)
+	{
+		if (prev)
+			prev->next = new_l;
+		else
+			cmd->args = new_l;
+		last_new = new_l;
+		while (last_new->next)
+			last_new = last_new->next;
+		last_new->next = curr->next;
+	}
+	else
+	{
+		if (prev)
+			prev->next = curr->next;
+		else
+			cmd->args = curr->next;
+	}
+}
+
+/* --- PARTIE 3 : REDIRECTIONS (AMBIGUOUS REDIRECT) --- */
 
 static int	expand_redir_file(t_redir *redir, t_shell *shell)
 {
 	char	*expanded;
 
-	// Si le fichier contient un $
 	if (ft_strchr(redir->file, '$'))
 	{
 		expanded = expand_text(redir->file, shell);
-		
-		// Vérifie si vide OU si contient des espaces (ambiguous redirect)
-		// Note: Pour être parfait (gérer "$VAR" avec espaces), il faudrait les segments.
-		// Mais ici on fait le check basique.
 		if (!expanded || ft_strlen(expanded) == 0 || ft_strchr(expanded, ' '))
 		{
 			printf("Minishell: %s: ambiguous redirect\n", redir->file);
 			if (expanded)
 				free(expanded);
-			
-			// === CORRECTION DOUBLE FREE ===
-			free(redir->file); 
-			redir->file = NULL; // Indispensable pour éviter le double free plus tard
-			// ==============================
-			
+			free(redir->file);
+			redir->file = NULL;
 			return (0);
 		}
 		free(redir->file);
@@ -81,8 +154,6 @@ static int	process_redirs(t_redir *lst, t_shell *shell)
 	status = 1;
 	while (lst)
 	{
-		// On continue même si erreur pour traiter tous les fichiers,
-		// mais on retiendra l'échec (0)
 		if (!expand_redir_file(lst, shell))
 			status = 0;
 		lst = lst->next;
@@ -90,30 +161,40 @@ static int	process_redirs(t_redir *lst, t_shell *shell)
 	return (status);
 }
 
+/* --- PARTIE 4 : MAIN EXPANDER --- */
+
 void	expander(t_shell *shell, t_command *cmd)
 {
-	t_command	*curr_cmd;
-	t_arg		*curr_arg;
+	t_command	*c;
+	t_arg		*curr;
+	t_arg		*prev;
+	t_arg		*next_save;
+	t_arg		*expanded_list;
 
-	curr_cmd = cmd;
-	while (curr_cmd)
+	c = cmd;
+	while (c)
 	{
-		curr_arg = curr_cmd->args;
-		while (curr_arg)
+		curr = c->args;
+		prev = NULL;
+		while (curr)
 		{
-			expand_arg(curr_arg, shell);
-			curr_arg = curr_arg->next;
+			next_save = curr->next;
+			expanded_list = expand_arg_to_list(curr, shell);
+			splice_args(c, prev, curr, expanded_list);
+			if (expanded_list)
+			{
+				prev = expanded_list;
+				while (prev->next != next_save)
+					prev = prev->next;
+			}
+			if (curr->value)
+				free(curr->value);
+			free(curr);
+			curr = next_save;
 		}
-
-		// Si ambiguous redirect détecté (retour 0), on met le status à 1.
-		// Grâce à la correction, redir->file est NULL, donc l'executor 
-		// échouera proprement sans créer de fichier.
-		if (!process_redirs(curr_cmd->input_redirection, shell)
-			|| !process_redirs(curr_cmd->output_redirection, shell))
-		{
+		if (!process_redirs(c->input_redirection, shell)
+			|| !process_redirs(c->output_redirection, shell))
 			shell->last_exit_status = 1;
-		}
-		
-		curr_cmd = curr_cmd->next;
+		c = c->next;
 	}
 }
